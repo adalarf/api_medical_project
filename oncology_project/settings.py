678f4ca57e47b7ab1c963d301a0122572439bf9a
Kirill S <zuzuwaq@gmail.com>
2024-05-26 18:31:55 +0500

    set ssl

--- oncology_project/settings.py
+++ oncology_project/settings.py
@@ -124,9 +124,13 @@ DATABASES = {
         'HOST': env("HOST"),
         'PORT': env("POST"),
         'OPTIONS': {
-            'sslmode': 'verify-full',
-            'sslrootcert': os.path.join(BASE_DIR, 'root.crt'),
-            'target_session_attrs': 'read-write',
+            #'sslmode': 'verify-full',
+            # 'sslrootcert': os.path.join(BASE_DIR, 'root.crt'),
+            # 'target_session_attrs': 'read-write',
+            'ssl':
+                {
+                    'key': os.path.join(BASE_DIR, 'root.crt')
+                }
         },
     }
 }
